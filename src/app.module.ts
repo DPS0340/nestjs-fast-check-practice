f5e2ee29486500b2d355f0e69e37cb93e8e1db74
Jiho Lee <optional.int@kakao.com>
2024-05-06 13:41:50 +0900

    chore: Remove unused app controller / service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AccountModule } from './infrastructure/ioc/account.module';
 
 @Module({
   imports: [AccountModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
