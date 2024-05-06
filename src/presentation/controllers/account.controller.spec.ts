a95c4ab65cc6fb1969b59ffdbba6ce034ec665af
Jiho Lee <optional.int@kakao.com>
2024-05-06 13:43:28 +0900

    feat: Implement AccountController test structure

---
+++ src/presentation/controllers/account.controller.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AccountController } from './account.controller';
+import { AccountsUseCases } from 'src/application/use-cases/account.use-case.';
+import { AccountEntity } from 'src/domain/entities/account.entity';
+import { AccountRepository } from 'src/infrastructure/persistence/account.repository';
+
+describe('AccountController', () => {
+  let accountController: AccountController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [AccountsUseCases, AccountEntity, AccountRepository],
+      controllers: [AccountController],
+      providers: [],
+    }).compile();
+
+    accountController = app.get<AccountController>(accountController);
+  });
+
+  describe('root', () => {
+    // it('should return "Hello World!"', () => {
+    //   expect(accountController.getHello()).toBe('Hello World!');
+    // });
+  });
+});
--- src/presentation/controllers/account.controller.ts
+++ src/presentation/controllers/account.controller.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AccountController {
-  private readonly logger = new Logger(AccountEntity.name);
+  private readonly logger = new Logger(AccountController.name);
 
   constructor() {}
 }
