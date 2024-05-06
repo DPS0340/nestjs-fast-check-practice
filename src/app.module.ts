799ad52ef19dc464cee4b02cb211590acbdedee4
Jiho Lee <optional.int@kakao.com>
2024-05-06 13:53:19 +0900

    feat: Init MikroOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AccountModule } from './infrastructure/ioc/account.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { SqliteDriver } from '@mikro-orm/sqlite';
 
 @Module({
-  imports: [AccountModule],
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/domain/entities'],
+      dbName: 'db.sqlite3',
+      driver: SqliteDriver,
+    }),
+    AccountModule,
+  ],
   controllers: [],
   providers: [],
 })
