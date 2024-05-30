69d62c74095de2c64236c7f434036e9bf680539f
Jiho Lee <optional.int@kakao.com>
2024-05-30 20:30:19 +0900

    chore: Delete unused imports

--- src/infrastructure/persistence/database.module.ts
+++ src/infrastructure/persistence/database.module.ts
@@ -1,8 +1,6 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { mikroOrmModuleConfig } from './mikro-orm.module.config';
-import { AccountEntity } from '../../domain/entities/account.entity';
-import { TransferEntity } from '../../domain/entities/transfer.entity';
 
 @Module({
   imports: [MikroOrmModule.forRoot(mikroOrmModuleConfig)],
